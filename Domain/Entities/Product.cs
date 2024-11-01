d0018273695618aa19462764210317df3d670761
mike-1208 <michaelkinsey1208@gmail.com>
2024-10-31 23:33:14 -0300

    feat: add Domain layer

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+namespace THETOURGUY.Domain.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public double AverageRating { get; set; }
+    public decimal RegularPrice { get; set; }
+    public decimal DiscountPrice { get; set; }
+    public int MaximumGuests { get; set; }
+
+    // Navigation Property
+    public ICollection<ProductImage> Images { get; set; } = new List<ProductImage>();
+
+
+}
+
---
+++ Domain/Entities/ProductImage.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace THETOURGUY.Domain.Entities;
+public class ProductImage
+{
+    public int Id { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public int DisplayOrder { get; set; }
+    public int ProductId { get; set; }    
+    [ForeignKey("ProductId")]
+    public Product? Product { get; set; }
+}
